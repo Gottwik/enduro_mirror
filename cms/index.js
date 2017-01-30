d533c9179281bafec301ac67c56b31ec76dc3106
Martin Gottweis <gottwik@gmail.com>
2017-01-30 16:06:13 +0100

    integrated with enduro

--- cms/index.js
+++ cms/index.js
@@ -3,7 +3,9 @@
 	specialty: 'Deep learning specialist',
 	$main_image_type: 'image',
 	main_image: 'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=1920&auto=compress&cs=tinysrgb',
+	$background_type: 'textarea',
 	background: 'I am a professional software developer with passion for deep learning. After finishing my computer science masters, I worked at Tim Dynamics at a autonomous submarine project. The amazing complexities of developing highly sofisticated diving model was astonishing. Sadly, after the submarine got stuck nearly 2 kilometers under the see surface, our funding was stopped. I became a freelance deep learning specialist and now enjoy diversity of projects ranging from medical research to video game AI.',
+	$projects_min: 1,
 	projects: [
 		{
 			title: 'Autonomous submarine',
@@ -63,5 +65,12 @@
 			text: 'I am also active at stackoverflow',
 			link: 'http://stackoverflow.com/cv/martingottweis'
 		}
-	]
+	],
+	hobby_background: 'https://images.pexels.com/photos/92323/pexels-photo-92323.jpeg?w=1920&auto=compress&cs=tinysrgb',
+	hobby_title: 'Book enthusiast',
+	hobby_subtitle: 'Books are the old-school internet',
+	cta_tagline: 'Need a deep learning software developer?',
+	cta_text: 'I am actively looking for new challenges',
+	contact_email: 'contact@endurojs.com',
+	footer_text: '2016 @enduro.js mirror theme'
 }
