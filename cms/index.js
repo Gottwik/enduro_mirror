280102fa35777136a899d132846b7606e7347a28
Martin Gottweis <gottwik@gmail.com>
2017-02-17 17:07:38 +0100

    added option to hide admin stamp

--- cms/index.js
+++ cms/index.js
@@ -2,7 +2,7 @@
 	name: 'Julia Smith',
 	specialty: 'Deep learning specialist',
 	$main_image_type: 'image',
-	main_image: 'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=1920&auto=compress&cs=tinysrgb',
+	main_image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880097_juliesmith_low.jpg',
 	$background_type: 'textarea',
 	background: 'I am a professional software developer with passion for deep learning. After finishing my computer science masters, I worked at Tim Dynamics at a autonomous submarine project. The amazing complexities of developing highly sofisticated diving model was astonishing. Sadly, after the submarine got stuck nearly 2 kilometers under the see surface, our funding was stopped. I became a freelance deep learning specialist and now enjoy diversity of projects ranging from medical research to video game AI.',
 	$projects_min: 1,
@@ -10,37 +10,37 @@
 		{
 			title: 'Autonomous submarine',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/27631/pexels-photo-27631.jpg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880340_pexels-photo-27631.jpg',
 			link: 'http://newatlas.com/boeing-echo-voyager/42272/'
 		},
 		{
 			title: 'Painting identification',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/20967/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880379_pexels-photo.jpg',
 			link: 'https://www.technologyreview.com/s/537366/the-machine-vision-algorithm-beating-art-historians-at-their-own-game/'
 		},
 		{
 			title: 'Drone obstacle identification',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/301871/pexels-photo-301871.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880400_pexels-photo-301871.jpeg',
 			link: 'http://www.neurala.com/drone-logistics-security/'
 		},
 		{
 			title: 'Machine learnt bird classification',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/132987/pexels-photo-132987.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880412_pexels-photo-132987.jpeg',
 			link: 'https://medium.com/@ageitgey/machine-learning-is-fun-part-3-deep-learning-and-convolutional-neural-networks-f40359318721#.otc8klxv3'
 		},
 		{
 			title: 'Deep learning meetup',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/57825/pexels-photo-57825.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880425_pexels-photo-57825.jpeg',
 			link: 'https://www.meetup.com/The-Deep-Learning-Group/'
 		},
 		{
 			title: 'Apartment hunting AI',
 			$image_type: 'image',
-			image: 'https://images.pexels.com/photos/279607/pexels-photo-279607.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880436_pexels-photo-279607.jpeg',
 			link: 'https://www.cs.cmu.edu/news/carnegie-mellon-project-uses-machine-learning-help-apartment-hunters-estimate-utility-costs'
 		}
 	],
@@ -66,7 +66,8 @@
 			link: 'http://stackoverflow.com/cv/martingottweis'
 		}
 	],
-	hobby_background: 'https://images.pexels.com/photos/92323/pexels-photo-92323.jpeg?w=1920&auto=compress&cs=tinysrgb',
+	$hobby_background_type: 'image',
+	hobby_background: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880478_pexels-photo-92323.jpeg',
 	hobby_title: 'Book enthusiast',
 	hobby_subtitle: 'Books are the old-school internet',
 	cta_tagline: 'Need a deep learning software developer?',
@@ -74,5 +75,6 @@
 	contact_email: 'contact@endurojs.com',
 	footer_text: '2016 @enduro.js mirror theme',
 	$footer_background_color_type: 'colorpicker',
-	footer_background_color: '#7e0343'
+	footer_background_color: '#7e0343',
+	display_edit_stamp: true
 }
